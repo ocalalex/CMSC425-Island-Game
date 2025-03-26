a91967d5e9a75d1f7492a983cf3565c0bc93867b
Renso0425 <darrenz0425@gmail.com>
2025-03-26 14:18:37 -0400

    Push Building
    building model created

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -6,16 +6,20 @@ public class Mover : MonoBehaviour
 {
     public Key fdKeyNum = Key.UpArrow;
     public Key bkKeyNum = Key.DownArrow;
+    public Key jmpKeyNum = Key.Space;
+    private bool onGround = true;
     [Header("Turning Keys")]
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public float speed = 10;
+    public float speed = 10f;
     KeyControl fdKey;
     KeyControl bkKey;
+    KeyControl jmpKey;
     float dps;
     void Start()
     {
         fdKey = Keyboard.current[fdKeyNum];
         bkKey = Keyboard.current[bkKeyNum];
+        jmpKey = Keyboard.current[jmpKeyNum];
     }
 
     // Update is called once per frame
@@ -24,10 +28,25 @@ public class Mover : MonoBehaviour
         if (fdKey.isPressed)
         {
             transform.Translate(0, 0, speed * Time.deltaTime);
+
         }
         if (bkKey.isPressed)
         {
             transform.Translate(0, 0, -speed * Time.deltaTime);
+        }
+        if (jmpKey.isPressed && onGround)
+        {
+            for (int i = 0; i < 30; i++){
+                transform.Translate(0, 10f * Time.deltaTime, 0);
+            }
+            onGround = false;
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
         }
     }
 }
