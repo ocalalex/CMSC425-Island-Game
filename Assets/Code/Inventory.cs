302b20cc6968a21464da48688746830b9b04d66e
ocalalex <oalex@umd.edu>
2025-04-08 20:55:38 -0400

    Made Inventory able to handle multiple items with different sprites, added gear for boat

--- Assets/Code/Inventory.cs
+++ Assets/Code/Inventory.cs
@@ -6,17 +6,24 @@ using System;
 
 public class Inventory : MonoBehaviour
 {
-    public Sprite itemImage;
+    //public Sprite itemImage;
     public Image[] inventory;
     private List<GameObject> items;
+    private List<Sprite> itemSprites;
     void Start()
     {
         items = new List<GameObject>();
+        itemSprites = new List<Sprite>();
         Display();
     }
 
     public void AddItem(GameObject item) {
         items.Add(item);
+
+        PickUp assignSprite = item.GetComponent<PickUp>();
+        itemSprites.Add(assignSprite.inventoryImage);
+        
+
         item.SetActive(false);
         Display();
     }
@@ -24,11 +31,16 @@ public class Inventory : MonoBehaviour
     public void UseItem(int i) {
         // Use key to open door
         items.RemoveAt(i);
+        itemSprites.RemoveAt(i);
         Display();
     }
 
     public void UseItem(GameObject item) {
         items.Remove(item);
+
+        PickUp assignSprite = item.GetComponent<PickUp>();
+        itemSprites.Remove(assignSprite.inventoryImage);
+
         Display();
     }
 
@@ -36,10 +48,22 @@ public class Inventory : MonoBehaviour
         return items.Contains(item);
     }
 
-    void Display() {
+    /* void Display() {
         for (int i = 0; i < inventory.Length; i++) {
             if (i < items.Count) {
                 inventory[i].sprite = itemImage;
+            } else {
+                inventory[i].sprite = null;
+            }
+        }
+    } */
+
+    void Display() {
+        for (int i = 0; i < inventory.Length; i++) {
+            if (i < items.Count) {
+
+                PickUp assignSprite = items[i].GetComponent<PickUp>();
+                inventory[i].sprite = assignSprite.inventoryImage;
             } else {
                 inventory[i].sprite = null;
             }
--- Assets/Code/PickUp.cs
+++ Assets/Code/PickUp.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
+    public Sprite inventoryImage;
     public Inventory inventory;
     public GameObject key;
     void OnMouseDown() {
