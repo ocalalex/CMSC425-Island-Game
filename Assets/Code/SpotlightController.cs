962a7f7e4760f7cbce2824f44418b2532a7aff7d
John N <106115048+JohnN05@users.noreply.github.com>
2025-04-10 14:31:30 -0400

    Potential fix for dialogue bug

--- Assets/Code/SpotlightController.cs
+++ Assets/Code/SpotlightController.cs
@@ -22,9 +22,15 @@ public class SpotlightDetection : MonoBehaviour
         Vector3 forwardDir = transform.forward;
         float coneRadius = Mathf.Tan(angleSpread * 0.5f * Mathf.Deg2Rad);
         float goldenRatio = (1 + Mathf.Sqrt(5)) / 2;
+        bool actionTriggered = false;
         
         //fibonacci spiral
         for(int i = 0; i < rayCount; i++){
+            //ensures only a single action is triggered for all rays
+            if(actionTriggered){
+                break;
+            }
+
             float theta = 2 * Mathf.PI * i / goldenRatio;  //angle
             float r = coneRadius * Mathf.Sqrt(i) / Mathf.Sqrt(rayCount);  //radius
             float x = r * Mathf.Cos(theta);
@@ -46,9 +52,11 @@ public class SpotlightDetection : MonoBehaviour
                             dialogue.TriggerSpottedLine();
                             teleporter.Teleport();
                             dialogue.isSpotted = false;
+                            actionTriggered = true;
+                            Debug.Log("Action triggered");
                         }
 
-                        Debug.Log("Spotlight detected: " + hit.collider.gameObject.name);
+                        // Debug.Log("Spotlight detected: " + hit.collider.gameObject.name);
                         if (debugRays)
                         {
                             Debug.DrawRay(transform.position, rayDir * hit.distance, Color.green);
