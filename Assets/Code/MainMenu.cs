5f035d248ef3631028fa5296f733f72e6bf6d31e
John N <106115048+JohnN05@users.noreply.github.com>
2025-05-03 00:23:18 -0400

    Finish Settings Menu

--- Assets/Code/MainMenu.cs
+++ Assets/Code/MainMenu.cs
@@ -8,6 +8,11 @@ public class MainMenu : MonoBehaviour
     public Camera startCamera;
     public Camera gameCamera;
     public UnityEvent playEvent;
+    AudioListener startListener;
+    AudioListener gameListener;
+
+    [Header("Debugging")]
+    public bool skipMenu = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -28,14 +33,39 @@ public class MainMenu : MonoBehaviour
         {
             Debug.LogError("Main Menu UI is not assigned in the inspector.");
         }
+        startListener = startCamera.GetComponent<AudioListener>();
+        if (startListener == null)
+        {
+            Debug.LogWarning("Start Camera doesn't contain AudioListener component.");
+        }
+        gameListener = gameCamera.GetComponent<AudioListener>();
+        if (gameListener == null)
+        {
+            Debug.LogWarning("Game Camera doesn't contain AudioListener component.");
+        }
+        if (skipMenu)
+        {
+            playGame();
+        }
+        else
+        {
+            showMainMenu();
+        }
+    }
+    public void showMainMenu()
+    {
         mainMenuUI.SetActive(true);
         startCamera.enabled = true;
         gameCamera.enabled = false;
+        startListener.enabled = true;
+        gameListener.enabled = false;
     }
 
     public void playGame(){
         startCamera.enabled = false;
         gameCamera.enabled = true;
+        startListener.enabled = false;
+        gameListener.enabled = true;
         playEvent.Invoke();
         mainMenuUI.SetActive(false);
     }
