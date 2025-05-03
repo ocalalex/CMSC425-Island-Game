d4dd7c1e7de9a6c4763c266d5be041172fba319e
John N <106115048+JohnN05@users.noreply.github.com>
2025-05-03 13:05:25 -0400

    Add Volume control to settings

---
+++ Assets/Code/MasterVolumeControl.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MasterVolumeControl : MonoBehaviour
+{
+    public AudioMixer masterMixer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(masterMixer == null)
+        {
+            Debug.LogError("Master Mixer is not assigned in the inspector.");
+            return;
+        }
+    }
+
+    public void updateMasterVolume(float vol){
+        float volumeDb = (vol > 0.001f) ? Mathf.Log10(vol) * 20f : -80f;
+
+        // Set the exposed parameter on the mixer
+        masterMixer.SetFloat("MasterVolume", volumeDb);
+    }
+}
