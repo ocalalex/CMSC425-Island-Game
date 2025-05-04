1c0a714369e89c5c2bf34cdf420936bdc4e857f7
John N <106115048+JohnN05@users.noreply.github.com>
2025-05-03 21:13:03 -0400

    Add possible in-game menu

--- Assets/Code/Settings.cs
+++ Assets/Code/Settings.cs
@@ -1,17 +1,55 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Settings : MonoBehaviour
 {
+    [Header("General Settings")]
     bool isVisible = false;
+    public bool inGame = false;
     public GameObject settingsMenu;
+    public KeyCode toggleKeyCode = KeyCode.Escape;
 
-    public void toggleSettingsMenu(){
+    [Header("In-Game Menu")]
+    public bool enableIngameMenu = true;
+    public Toggler gameplayToggler;
+
+    public void Start()
+    {
+        if(settingsMenu == null)
+        {
+            Debug.LogError("Settings menu is not assigned in the inspector.");
+        }
+        if(gameplayToggler == null)
+        {
+            Debug.LogError("Gameplay toggler is not assigned in the inspector.");
+        }
+    }
+
+    public void Update()
+    {
+        if(enableIngameMenu){
+            HotKeyToggleSettingsMenu();
+        }
+    }
+
+    public void ToggleSettingsMenu(){
         if(isVisible){
             settingsMenu.SetActive(false);
             isVisible = false;
         }else{
             settingsMenu.SetActive(true);
             isVisible = true;
+        }
+    }
+
+    public void updateGameStatus(bool status){
+        inGame = status;
+    }
+    public void HotKeyToggleSettingsMenu(){
+        if(inGame && Input.GetKeyDown(toggleKeyCode)){
+            ToggleSettingsMenu();
+            gameplayToggler.toggleActions();
         }
     }
 }
--- Assets/Code/Toggler.cs
+++ Assets/Code/Toggler.cs
@@ -43,4 +43,5 @@ public class Toggler : MonoBehaviour
         }
         
     }
+    
 }
