cb66645e157cead98e49fbc6598c8ba5f9999d5e
nataliechen419 <nataliechen419@gmail.com>
2025-05-04 12:28:55 -0400

    Cane exchange for toolbox to grandma finally fixed

--- Assets/Code/CaneToGrandma.cs
+++ Assets/Code/CaneToGrandma.cs
@@ -1,18 +1,19 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CaneToGrandma : MonoBehaviour
 {
     public Inventory inventory;
     public GameObject toolbox;
-    public LayerMask interactLayer; // Layer Grandma is on
+    public GameObject cane;
     public float clickRadius = 20f; 
+    private int objectsLayer;
 
     private Camera mainCam;
 
     void Start()
     {
-        mainCam = Camera.main;
-
+        objectsLayer = LayerMask.GetMask("Objects"); 
         if (inventory == null)
             Debug.LogWarning("Inventory not assigned to CaneToGrandma.");
         if (toolbox == null)
@@ -23,30 +24,26 @@ public class CaneToGrandma : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit, 20f, interactLayer))
-            {
-                if (hit.collider.gameObject.name == "Grandma")
-                {
+            if (Physics.Raycast(ray, out hit, clickRadius, objectsLayer)) {
+                if (hit.transform == transform) {
                     Debug.Log("Clicked on Grandma");
-
-                    if (inventory.CheckItem(gameObject)) // this cane
+                    if (inventory.CheckItem(cane)) // see if user have cane to give to grandma
                     {
                         Debug.Log("Grandma received the cane");
 
-                        inventory.UseItem(gameObject);   // remove cane
+                        inventory.UseItem(cane);   // remove cane
                         inventory.AddItem(toolbox);      // add toolbox
 
-                        // Optional: destroy cane object from scene
-                        Destroy(gameObject);
                     }
                     else
                     {
                         Debug.Log("Cane not in inventory");
                     }
-                }
+                
+                 }
             }
         }
     }
