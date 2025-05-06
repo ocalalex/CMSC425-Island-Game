cac25c2f4f8b4091c0ce286592fe1c15a016407a
Renso0425 <darrenz0425@gmail.com>
2025-05-06 00:24:01 -0400

    Updated Map

--- Assets/Code/Mapper.cs
+++ Assets/Code/Mapper.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
 
+/*This file is to create the map item functionality when clicking M*/
+
 public class Mapper : MonoBehaviour
 {
 
     public Inventory inventory;
-    public GameObject map;
-    public GameObject userMarker;
-    public GameObject keyMarkerText, keyMarkerDot, spawnMarkerText, spawnMarkerDot, grannyMarkerText,
+    public GameObject map, userMarker, keyMarkerText, keyMarkerDot, spawnMarkerText, spawnMarkerDot, grannyMarkerText,
      grannyMarkerDot, cargoMarkerText, cargoMarkerDot, factMarkerText, factMarkerDot, lightMarkerText, lightMarkerDot, boatMarkerText, boatMarkerDot;
 
     public Key mapKeyNum = Key.M;
@@ -36,8 +36,9 @@ public class Mapper : MonoBehaviour
         boatMarkerText.GetComponent<Renderer>().enabled = false;
         boatMarkerDot.GetComponent<Renderer>().enabled = false;
         userMarker.GetComponent<Renderer>().enabled = false;
+        mainCamera.enabled = true;
+        mapCamera.enabled = false;
         mapKey = Keyboard.current[mapKeyNum];
-        
     }
 
     // Update is called once per frame
@@ -47,48 +48,23 @@ public class Mapper : MonoBehaviour
         {
             if (inventory.CheckItem(map)) 
             {
-                if (inMapView)
-                {
-                    inMapView = false;
-                    keyMarkerText.GetComponent<Renderer>().enabled = false;
-                    keyMarkerDot.GetComponent<Renderer>().enabled = false;
-                    userMarker.GetComponent<Renderer>().enabled = false;
-                    spawnMarkerText.GetComponent<Renderer>().enabled = false;
-                    spawnMarkerDot.GetComponent<Renderer>().enabled = false;
-                    grannyMarkerText.GetComponent<Renderer>().enabled = false;
-                    grannyMarkerDot.GetComponent<Renderer>().enabled = false;
-                    cargoMarkerText.GetComponent<Renderer>().enabled = false;
-                    cargoMarkerDot.GetComponent<Renderer>().enabled = false;
-                    factMarkerText.GetComponent<Renderer>().enabled = false;
-                    factMarkerDot.GetComponent<Renderer>().enabled = false;
-                    lightMarkerText.GetComponent<Renderer>().enabled = false;
-                    lightMarkerDot.GetComponent<Renderer>().enabled = false;
-                    boatMarkerText.GetComponent<Renderer>().enabled = false;
-                    boatMarkerDot.GetComponent<Renderer>().enabled = false;
-                    userMarker.GetComponent<Renderer>().enabled = false;
-                    mainCamera.enabled = true;
-                    mapCamera.enabled = false;
-                } else {
-                    inMapView = true;
-                    mainCamera.enabled = false;
-                    keyMarkerText.GetComponent<Renderer>().enabled = true;
-                    keyMarkerDot.GetComponent<Renderer>().enabled = true;
-                    userMarker.GetComponent<Renderer>().enabled = true;
-                    spawnMarkerText.GetComponent<Renderer>().enabled = true;
-                    spawnMarkerDot.GetComponent<Renderer>().enabled = true;
-                    grannyMarkerText.GetComponent<Renderer>().enabled = true;
-                    grannyMarkerDot.GetComponent<Renderer>().enabled = true;
-                    cargoMarkerText.GetComponent<Renderer>().enabled = true;
-                    cargoMarkerDot.GetComponent<Renderer>().enabled = true;
-                    factMarkerText.GetComponent<Renderer>().enabled = true;
-                    factMarkerDot.GetComponent<Renderer>().enabled = true;
-                    lightMarkerText.GetComponent<Renderer>().enabled = true;
-                    lightMarkerDot.GetComponent<Renderer>().enabled = true;
-                    boatMarkerText.GetComponent<Renderer>().enabled = true;
-                    boatMarkerDot.GetComponent<Renderer>().enabled = true;
-                    userMarker.GetComponent<Renderer>().enabled = true;
-                    mapCamera.enabled = true;
-                }
+                keyMarkerText.GetComponent<Renderer>().enabled = !keyMarkerText.GetComponent<Renderer>().enabled;
+                keyMarkerDot.GetComponent<Renderer>().enabled = !keyMarkerDot.GetComponent<Renderer>().enabled;
+                userMarker.GetComponent<Renderer>().enabled = !userMarker.GetComponent<Renderer>().enabled;
+                spawnMarkerText.GetComponent<Renderer>().enabled = !spawnMarkerText.GetComponent<Renderer>().enabled;
+                spawnMarkerDot.GetComponent<Renderer>().enabled = !spawnMarkerDot.GetComponent<Renderer>().enabled;
+                grannyMarkerText.GetComponent<Renderer>().enabled = !grannyMarkerText.GetComponent<Renderer>().enabled;
+                grannyMarkerDot.GetComponent<Renderer>().enabled = !grannyMarkerDot.GetComponent<Renderer>().enabled;
+                cargoMarkerText.GetComponent<Renderer>().enabled = !cargoMarkerText.GetComponent<Renderer>().enabled;
+                cargoMarkerDot.GetComponent<Renderer>().enabled = !cargoMarkerDot.GetComponent<Renderer>().enabled;
+                factMarkerText.GetComponent<Renderer>().enabled = !factMarkerText.GetComponent<Renderer>().enabled;
+                factMarkerDot.GetComponent<Renderer>().enabled = !factMarkerDot.GetComponent<Renderer>().enabled;
+                lightMarkerText.GetComponent<Renderer>().enabled = !lightMarkerText.GetComponent<Renderer>().enabled;
+                lightMarkerDot.GetComponent<Renderer>().enabled = !lightMarkerDot.GetComponent<Renderer>().enabled;
+                boatMarkerText.GetComponent<Renderer>().enabled = !boatMarkerText.GetComponent<Renderer>().enabled;
+                boatMarkerDot.GetComponent<Renderer>().enabled = !boatMarkerDot.GetComponent<Renderer>().enabled;
+                mainCamera.enabled = !mainCamera.enabled;
+                mapCamera.enabled = !mapCamera.enabled;
             }
         }
         
